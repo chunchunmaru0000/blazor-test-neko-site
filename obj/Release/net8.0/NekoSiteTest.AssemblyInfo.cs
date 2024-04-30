98d5dad2ad7a4577cf1d7d95eadf369c8b903f4d
chunchunmaru <cho227675m@gmail.com>
2024-04-30 11:40:58 +0300

    да вот так

--- obj/Release/net8.0/NekoSiteTest.AssemblyInfo.cs
+++ obj/Release/net8.0/NekoSiteTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NekoSiteTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe713ef73c51d96dc4edaeb7dffe686d44b9a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ff895ef966044f03bc584597627f74972a577e")]
 [assembly: System.Reflection.AssemblyProductAttribute("NekoSiteTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NekoSiteTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
